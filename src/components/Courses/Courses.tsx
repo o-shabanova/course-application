33cda0ceadf2b1c490369a449761499b961c24fc
Olya Shabanova <olha.shbnv@gmail.com>
2025-10-25 23:56:49 +0300

    Implement and style Searchbar component

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -3,16 +3,20 @@ import './Courses.css';
 import CourseCard from './components/CourseCard/CourseCard';
 import { mockedCoursesList, mockedAuthorsList, BUTTON_TEXT} from '../../constants';
 import Button from '../../common/Button/Button';
+import SearchBar from './components/SearchBar/SearchBar';
 
 const Courses: React.FC = () => {
   return (
     <main className="courses-container">
-    <Button buttonText={BUTTON_TEXT.ADD_NEW_COURSE} type="button" className="main-button" />
-    <ul className="courses-list">
-      {mockedCoursesList.map((course) => (
-        <CourseCard key={course.id} course={course} authorNames={mockedAuthorsList.filter((author) => course.authors.includes(author.id)).map((author) => author.name)} />
-      ))}
-    </ul>
+        <div className="courses-header">
+            <SearchBar />
+            <Button buttonText={BUTTON_TEXT.ADD_NEW_COURSE} type="button" className="main-button add-new-course-button" />
+        </div>
+        <ul className="courses-list">
+            {mockedCoursesList.map((course) => (
+                <CourseCard key={course.id} course={course} authorNames={mockedAuthorsList.filter((author) => course.authors.includes(author.id)).map((author) => author.name)} />
+            ))}
+        </ul>
     </main>
   );
 };
--- src/components/Courses/components/SearchBar/SearchBar.tsx
+++ src/components/Courses/components/SearchBar/SearchBar.tsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import { BUTTON_TEXT } from '../../../../constants';
+import Button from '../../../../common/Button/Button';
+import './SearchBar.css';
 
+const SearchBar: React.FC = () => {
+  return (
+    <div className="search-bar-container">
+        <input type="text" placeholder="Search" className="search-bar-input" />
+        <Button buttonText={BUTTON_TEXT.SEARCH} type="button" className="main-button search-bar-button" />
+    </div>
+  )
+};
+export default SearchBar;
