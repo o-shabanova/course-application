bd9c5bf004b1194353f4cc94c4e242a99c1e7fd4
Olya Shabanova <olha.shbnv@gmail.com>
2025-10-26 11:00:05 +0200

    Move mocked data to App component and pass to it's child as a props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Header } from './components/Header/Header';
 import Courses from './components/Courses/Courses';
+import { mockedCoursesList, mockedAuthorsList} from './constants';
 
 function App() {
   return (
     <>
     <Header />
-    <Courses />
+    <Courses courses={mockedCoursesList} authors={mockedAuthorsList} />
     </>
   );
 }
--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import './Courses.css';
 import CourseCard from './components/CourseCard/CourseCard';
-import { mockedCoursesList, mockedAuthorsList, BUTTON_TEXT} from '../../constants';
+import {BUTTON_TEXT} from '../../constants';
 import Button from '../../common/Button/Button';
 import SearchBar from './components/SearchBar/SearchBar';
 
-const Courses: React.FC = () => {
+interface Author {
+    id: string;
+    name: string;
+  }
+  
+  interface Course {
+    id: string;
+    title: string;
+    description: string;
+    creationDate: string;
+    duration: number;
+    authors: string[];
+  }
+  
+  interface CoursesProps {
+    courses: Course[];
+    authors: Author[];
+  }
+
+const Courses: React.FC<CoursesProps> = ({courses, authors}) => {
   return (
     <main className="courses-container">
         <div className="courses-header">
@@ -13,8 +32,9 @@ const Courses: React.FC = () => {
             <Button buttonText={BUTTON_TEXT.ADD_NEW_COURSE} type="button" className="main-button add-new-course-button" />
         </div>
         <ul className="courses-list">
-            {mockedCoursesList.map((course) => (
-                <CourseCard key={course.id} course={course} authorNames={mockedAuthorsList.filter((author) => course.authors.includes(author.id)).map((author) => author.name)} />
+            {courses.map((course) => (<li key={course.id} className="course-list-item">
+                <CourseCard key={course.id} course={course} authorNames={authors.filter((author) => course.authors.includes(author.id)).map((author) => author.name)} />
+                </li>
             ))}
         </ul>
     </main>
