374195fd62c228f285a9596b79af621ed6380d14
Olya Shabanova <olha.shbnv@gmail.com>
2025-10-26 11:49:14 +0200

    Implement EmptyCourseList component and it's styles, implement condition logic for the App component when it needs to render either the Courses component or the EmptyCourseList

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Header } from './components/Header/Header';
 import Courses from './components/Courses/Courses';
 import { mockedCoursesList, mockedAuthorsList} from './constants';
+import EmptyCourseList from './components/EmptyCourseList/EmptyCourseList';
 
 function App() {
   return (
     <>
     <Header />
-    <Courses courses={mockedCoursesList} authors={mockedAuthorsList} />
+    {(mockedCoursesList.length > 0) ? <Courses courses={mockedCoursesList} authors={mockedAuthorsList} /> : <EmptyCourseList />}
     </>
   );
 }
--- src/components/EmptyCourseList/EmptyCourseList.tsx
+++ src/components/EmptyCourseList/EmptyCourseList.tsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import { BUTTON_TEXT } from '../../constants';
+import { Button } from '../../common/Button/Button';
+import './EmptyCourseList.css';
 
+const EmptyCourseList:React.FC = () => {
+    return (
+        <main className="empty-course-list">
+            <div className="empty-course-list-content">
+                <h3 className="empty-course-list-title">Course List is Empty</h3>
+                <p className="empty-course-list-description">Please use "Add New Course" button to add your first course</p>
+                <Button buttonText={BUTTON_TEXT.ADD_NEW_COURSE} type="button" className="main-button add-new-course-button" />
+            </div>
+        </main>
+    )
+}
+export default EmptyCourseList;
