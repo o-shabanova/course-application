d3ad6f5505f17343d442c53fe56772d3b7a10583
Olya Shabanova <olha.shbnv@gmail.com>
2025-10-26 17:48:17 +0200

    Implement CourseInfo component with it's styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Header } from './components/Header/Header';
-import Courses from './components/Courses/Courses';
+// import Courses from './components/Courses/Courses';
 import { mockedCoursesList, mockedAuthorsList} from './constants';
-import EmptyCourseList from './components/EmptyCourseList/EmptyCourseList';
-
+// import EmptyCourseList from './components/EmptyCourseList/EmptyCourseList';
+import CourseInfo from './components/CourseInfo/CourseInfo';
 function App() {
   return (
     <>
     <Header />
-    {(mockedCoursesList.length > 0) ? <Courses courses={mockedCoursesList} authors={mockedAuthorsList} /> : <EmptyCourseList />}
+    {/* {(mockedCoursesList.length > 0) ? <Courses courses={mockedCoursesList} authors={mockedAuthorsList} /> : <EmptyCourseList />} */}
+    <CourseInfo course={mockedCoursesList[0]} authors={mockedAuthorsList} />
     </>
   );
 }
--- src/components/CourseInfo/CourseInfo.tsx
+++ src/components/CourseInfo/CourseInfo.tsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import { Button } from '../../common/Button/Button';
+import { BUTTON_TEXT } from '../../constants';
+import { formatCreationDate } from '../../helpers/formatCreationDate';
+import { getCourseDuration } from '../../helpers/getCourseDuration';
+import './CourseInfo.css';
+interface Author {
+  id: string;
+  name: string;
+}
 
-const CourseInfo = () => {
-  return <div>CourseInfo</div>;
+interface Course {
+  id: string;
+  title: string;
+  description: string;
+  creationDate: string;
+  duration: number;
+  authors: string[];
+}
+
+interface CourseInfoProps {
+  course: Course;
+  authors: Author[];
+}
+const CourseInfo:React.FC<CourseInfoProps> = ({course, authors}) => {
+  const formattedCourseDuration = getCourseDuration(course.duration);
+  const formattedCreationDate = formatCreationDate(course.creationDate);
+  
+  const authorNames = course.authors.map(authorId => 
+    authors.find(author => author.id === authorId)?.name
+  ).filter(Boolean).join(', ');
+
+  return (
+    <main className="course-info-container">
+      <article className="course-info-content">
+      <h1 className="course-info-card-title">{course.title}</h1>
+      <div className="course-info-card-content">
+          <div className="course-info-card-content-description">
+            <p className="course-info-card-description-title"><span className="course-info-description-label">Description:</span></p>
+            <p className="course-info-card-description-text">{course.description}</p>
+          </div>
+          <div className="course-info-card-content-details">
+            <p className="course-info-card-id"><span className="course-info-card-label">ID:</span> {course.id}</p>
+            <p className="course-info-card-duration"><span className="course-info-card-label">Duration:</span> {formattedCourseDuration}</p>
+            <p className="course-info-card-creation-date"><span className="course-info-card-label">Created:</span> {formattedCreationDate}</p>
+            <p className="course-info-card-authors"><span className="course-info-card-label">Authors:</span> {authorNames}</p>
+          </div>     
+      </div>
+      <Button buttonText={BUTTON_TEXT.BACK} type="button" className="main-button back-button" />
+      </article>
+    </main>
+  );
 };
 
 export default CourseInfo;
