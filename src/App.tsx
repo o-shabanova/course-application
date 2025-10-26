7e23f89d3edca58845e329e2c57070056b905dfd
Olya Shabanova <olha.shbnv@gmail.com>
2025-10-26 18:03:12 +0200

    Uncomment important code and remove redundant code and comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { Header } from './components/Header/Header';
-// import Courses from './components/Courses/Courses';
+import Courses from './components/Courses/Courses';
 import { mockedCoursesList, mockedAuthorsList} from './constants';
-// import EmptyCourseList from './components/EmptyCourseList/EmptyCourseList';
-import CourseInfo from './components/CourseInfo/CourseInfo';
+import EmptyCourseList from './components/EmptyCourseList/EmptyCourseList';
+
 function App() {
   return (
     <>
     <Header />
-    {/* {(mockedCoursesList.length > 0) ? <Courses courses={mockedCoursesList} authors={mockedAuthorsList} /> : <EmptyCourseList />} */}
-    <CourseInfo course={mockedCoursesList[0]} authors={mockedAuthorsList} />
+    {(mockedCoursesList.length > 0) ? <Courses courses={mockedCoursesList} authors={mockedAuthorsList} /> : <EmptyCourseList />}
     </>
   );
 }
