a04799ca2f8c6534e2457ebc5c6ab24e47ee7195
Olya Shabanova <olha.shbnv@gmail.com>
2025-10-31 23:33:27 +0200

    Styling labels in Registration component, add classes to Inputs and labelClassName to InputProps

--- src/common/Input/Input.tsx
+++ src/common/Input/Input.tsx
@@ -9,12 +9,13 @@ interface InputProps {
     value: string,  
     onChange: (value: string) => void,
     required: boolean,
-    className?: string
+    className?: string,
+    labelClassName?: string
 }
 
-export const Input: React.FC<InputProps> = ({ labelText, type, placeholderText, value, onChange, required, className }) =>  (
+export const Input: React.FC<InputProps> = ({ labelText, type, placeholderText, value, onChange, required, className, labelClassName }) =>  (
     <div className={className}>
-        <label>{labelText}</label>
+        <label className={labelClassName}>{labelText}</label>
         <input className="input" type={type} placeholder={placeholderText} value={value} onChange={(e) => onChange(e.target.value)} required={required} />
     </div>
 );
--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -16,11 +16,11 @@ export const Registration: React.FC<RegistrationProps> = ({ title, onSubmit }) =
         <h2 className="registration-title">{title}</h2>
             <fieldset className="registration-fieldset">
                 <div className="registration-content">
-                    <Input className="registration-input name-input" labelText="Name" type="text" placeholderText="Enter your name" value="" onChange={() => {}} required={true} />
-                    <Input className="registration-input email-input" labelText="Email" type="email" placeholderText="Enter your email" value="" onChange={() => {}} required={true} />
-                    <Input className="registration-input password-input" labelText="Password" type="password" placeholderText="Enter your password" value="" onChange={() => {}} required={true} />
+                    <Input className="registration-input name-input" labelClassName="registration-label" labelText="Name" type="text" placeholderText="Enter your name" value="" onChange={() => {}} required={true} />
+                    <Input className="registration-input email-input" labelClassName="registration-label" labelText="Email" type="email" placeholderText="Enter your email" value="" onChange={() => {}} required={true} />
+                    <Input className="registration-input password-input" labelClassName="registration-label" labelText="Password" type="password" placeholderText="Enter your password" value="" onChange={() => {}} required={true} />
                     <Button buttonText={BUTTON_TEXT.REGISTER} type="submit" onClick={onSubmit} className="main-button registration-button" />
-                    <p className="registration-paragraph">If you have an account you may <span>Login</span></p>
+                    <p className="registration-paragraph">If you have an account you may <span className="registration-link">Login</span></p>
                 </div>
             </fieldset>
         </form>
