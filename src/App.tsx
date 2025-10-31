d709598f26df5e47881895a8beaaada2cab27450
Olya Shabanova <olha.shbnv@gmail.com>
2025-10-31 20:37:35 +0200

    Add Registration component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-
 import { Header } from './components/Header/Header';
 import Courses from './components/Courses/Courses';
 import { mockedCoursesList, mockedAuthorsList} from './constants';
 import EmptyCourseList from './components/EmptyCourseList/EmptyCourseList';
-
+import { Registration } from './components/Registration/Registration';
 
 function App() {
   return (
     <>
-
     <Header />
     {(mockedCoursesList.length > 0) ? <Courses courses={mockedCoursesList} authors={mockedAuthorsList} /> : <EmptyCourseList />}
+    <Registration title="Registration"  buttonText="Register" onSubmit={() => {}} />
     </>
   );
 }
---
+++ src/components/Registration/Registration.tsx
@@ -0,0 +1,26 @@
+import React from 'react'; 
+import './Registration.css';
+import {Input } from '../Input/Input';
+
+interface RegistrationProps {
+    title: string;
+    buttonText: string;
+    onSubmit: () => void;
+}
+
+export const Registration: React.FC<RegistrationProps> = ({ title, buttonText, onSubmit }) => {
+    return (
+        <>
+        <form className="registration-container">
+            <fieldset>
+                <legend>{title}</legend>
+                <Input labelText="Name" type="text" placeholderText="Enter your name" value="" onChange={() => {}} required={true} />
+                <Input labelText="Email" type="email" placeholderText="Enter your email" value="" onChange={() => {}} required={true} />
+                <Input labelText="Password" type="password" placeholderText="Enter your password" value="" onChange={() => {}} required={true} />
+            <button className="registration-button" onClick={onSubmit}>{buttonText}</button>
+            </fieldset>
+        </form>
+        <p>If you have an account you may <span>Login</span></p>
+        </>
+    )
+}
