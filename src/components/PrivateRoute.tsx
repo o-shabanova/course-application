9098a700f4c7da440900880c2d81fbd4665296cb
Olya Shabanova <olha.shbnv@gmail.com>
2025-11-29 17:30:07 +0200

    Set task 3 prerequisites

---
+++ src/components/PrivateRoute.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import { Navigate } from 'react-router-dom';
+
+const PrivateRoute: FC<{ children: React.ReactNode }> = ({ children }) => {
+  const token = localStorage.getItem('token');
+  return token ? <>{children}</> : <Navigate to="/login" replace />;
+};
+
+export default PrivateRoute;
