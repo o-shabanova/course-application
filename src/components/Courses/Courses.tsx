3d1f05b5713effb8a4d121a53f57372cfa3a2c2e
Olya Shabanova <olha.shbnv@gmail.com>
2025-12-03 15:00:25 +0200

    fetch authors and courses data from the API

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -1,35 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './Courses.css';
 import CourseCard from './components/CourseCard/CourseCard';
-import {BUTTON_TEXT, mockedCoursesList, mockedAuthorsList} from '../../constants';
+import { BUTTON_TEXT, API_BASE_URL } from '../../constants';
 import Button from '../../common/Button/Button';
 import SearchBar from './components/SearchBar/SearchBar';
 import getAuthorsNames from '../../helpers/getAuthorsNames';
 import { Link } from 'react-router-dom';
+import { RootState, AppDispatch } from '../../store';
+import { setCourses } from '../../store/courses/coursesSlice';
+import { setAuthors } from '../../store/authors/authorsSlice';
 
+const Courses: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const courses = useSelector((state: RootState) => state.courses);
+  const authors = useSelector((state: RootState) => state.authors);
 
-interface Author {
-    id: string;
-    name: string;
-  }
-  
-  interface Course {
-    id: string;
-    title: string;
-    description: string;
-    creationDate: string;
-    duration: number;
-    authors: string[];
-  }
-  
-  interface CoursesProps {
-    courses: Course[];
-    authors: Author[];
-  }
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await fetch(`${API_BASE_URL}/courses/all`);
+        if (response.ok) {
+          const data = await response.json();
+          const courses = Array.isArray(data.result) ? data.result : [];
+          dispatch(setCourses(courses));
+        }
+      } catch (error) {
+        console.error('Failed to fetch courses:', error);
+      }
+    };
 
-const Courses: React.FC<CoursesProps> = ({courses = mockedCoursesList, authors = mockedAuthorsList}) => {
+    const fetchAuthors = async () => {
+      try {
+        const response = await fetch(`${API_BASE_URL}/authors/all`);
+        if (response.ok) {
+          const data = await response.json();
+          const authors = Array.isArray(data.result) ? data.result : [];
+          dispatch(setAuthors(authors));
+        }
+      } catch (error) {
+        console.error('Failed to fetch authors:', error);
+      }
+    };
 
-  
+    fetchCourses();
+    fetchAuthors();
+  }, [dispatch]);
+
   return (
     <div className="courses-container">
         <div className="courses-header">
