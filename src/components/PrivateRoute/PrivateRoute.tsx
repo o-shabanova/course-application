08ff9f814b9657e47896e3a1e92567db7829b29a
Olya Shabanova <olha.shbnv@gmail.com>
2025-12-04 23:43:45 +0200

    Change token usage in PrivateRoute, get isAuth checking using userSlice

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store';
 
 const PrivateRoute: FC<{ children: React.ReactNode }> = ({ children }) => {
-  const token = localStorage.getItem('token');
-  return token ? <>{children}</> : <Navigate to="/login" replace />;
+  const isAuth = useSelector((state: RootState) => state.user.isAuth);
+  return isAuth ? <>{children}</> : <Navigate to="/login" replace />;
 };
 
 export default PrivateRoute;
