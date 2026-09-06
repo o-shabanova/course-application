2086ca5db34acfb6e6b3e9a6eb2053a1305662c9
Olya Shabanova <olha.shbnv@gmail.com>
2026-09-06 23:16:16 +0300

    Implementing a new feature for EmptyCourseList component: - for admin user: By clicking add-new-course button user should be navigated to he route courses/add - for non-admin users: Caution message is displayed instead of add-new-course button

--- src/components/EmptyCourseList/EmptyCourseList.tsx
+++ src/components/EmptyCourseList/EmptyCourseList.tsx
@@ -14,11 +14,17 @@ const EmptyCourseList:React.FC = () => {
         <main className="empty-course-list">
             <div className="empty-course-list-content">
                 <h3 className="empty-course-list-title">Course List is Empty</h3>
-                <p className="empty-course-list-description">Please use "Add New Course" button to add your first course</p>
-                {isAdmin && (
-                  <Link to="/courses/add">
-                    <Button buttonText={BUTTON_TEXT.ADD_NEW_COURSE} type="button" className="main-button add-new-course-button" />
-                  </Link>
+                {isAdmin ? (
+                  <>
+                    <p className="empty-course-list-description">Please use "Add New Course" button to add your first course</p>
+                    <Link to="/courses/add">
+                      <Button buttonText={BUTTON_TEXT.ADD_NEW_COURSE} type="button" className="main-button add-new-course-button" />
+                    </Link>
+                  </>
+                ) : (
+                  <p className="empty-course-list-description">
+                    You don't have permissions to create a course. Please log in as ADMIN
+                  </p>
                 )}
             </div>
         </main>
